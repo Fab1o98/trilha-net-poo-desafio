80d79356c3a40ce3dfed7d24b71e9cc3b36bc13e
Fabio <fabiio1262@gmail.com>
2024-01-09 11:31:05 -0300

    Desafio-DIO-Concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,24 @@
+using System.Globalization;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
 
-        public Smartphone(string numero)
+        private string IMEI { get; set; } 
+
+        private int Memoria { get; set; } 
+
+        public Smartphone(string numero, string imei, string modelo, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            IMEI = imei;
+            Modelo = modelo;
+            Memoria = memoria; 
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia X4:"); 
+Smartphone nokia = new Nokia(numero : "(17)989456512", imei : "111111111", modelo: "X4", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook"); 
+
+
+
+Console.WriteLine("\n"); 
+
+
+Console.WriteLine("Iphone 13 PRO:"); 
+Smartphone iphone = new Iphone(numero : "(11)912213333", imei : "0110110001", modelo: "13 PRO", memoria: 512); 
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Twitch"); 
